f9841acd49c0a33e837eb158c5a584e2e2757d48
Qilton <swayambhalotia@gmail.com>
2025-09-06 18:16:09 +0530

    Backend done?

--- src/app.ts
+++ src/app.ts
@@ -12,14 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Auth routes
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 app.use('/family', familyRoutes);
 app.use('/contacts', contactRoutes);
 app.use('/bills', billRoutes);
 app.use('/monthly-payment', monthlyPaymentRoutes);
-// Global error handler (should be after routes)
 app.use(errorHandler);
 
 export default app;
